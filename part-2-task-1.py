4a36f1c43aba6d7239b0030224a8c05cb343a954
Edala, Mohan <mohan.edala@anthem.com>
2019-01-20 19:18:58 -0800

    initialized an integer list with name list1 and store the first 8 multiples of number 7 in the list and printed the  contents as per the requirement

---
+++ part-2-task-1.py
@@ -0,0 +1,5 @@
+list1 = [1, 2, 3, 4, 5, 6, 7, 8]
+print("Below is the list of first", len(list1), "multiple of number 7")
+print('\n', "1st multiple:", 7*list1[0], '\n', "2nd multiple:", 7*list1[1], '\n', "3rd multiple:", 7*list1[2], '\n',
+      "4th multiple:", 7*list1[3], '\n', "5th multiple:", 7 * list1[4], '\n', "6th multiple:", 7*list1[5], '\n',
+      "7th multiple:", 7*list1[6], '\n', "8th multiple:", 7*list1[7])
