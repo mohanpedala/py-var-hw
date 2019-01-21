3be1dda3cec6cc6c56a01a5c5fd127cadb85dfd7
Edala, Mohan <mohan.edala@anthem.com>
2019-01-20 19:39:40 -0800

    produced the required output

---
+++ part-2-task-2.py
@@ -0,0 +1,6 @@
+list1 = ['this is string', 'momma is megha', 'hello to hello', 'string count 4',
+         'string count 5', 'string count 6 ', 'end is here']
+
+print('\n', "Below is the list of random worded strings", '\n', "1st String:", list1[0], '\n', "2nd String:", list1[1], '\n', "3rd String:", list1[2],
+      '\n', "4th String:", list1[3], '\n', "5th String:", list1[4], '\n', "6th String:", list1[5],
+      '\n', "7th String:", list1[6],)
