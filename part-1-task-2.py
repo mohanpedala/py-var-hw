32e60c14b28557d53dd0dc48853f13e0fc0c335b
Edala, Mohan <mohan.edala@anthem.com>
2019-01-20 18:41:34 -0800

    completed string initialization

---
+++ part-1-task-2.py
@@ -0,0 +1,12 @@
+str1 = "Contents of first string variable"
+str2 = "merry christmas"
+str3 = "and happy new year."
+str4 = "welcome to 2019"
+str5 = "Started"
+str6 = "my programming"
+str7 = "career"
+str8 = "with python"
+str9 = "this could be it"
+str10 = "for a new start"
+
+print(str1, str2, str3, str4, str5, str6, str7, str8, str9, str10)
