c2b66a295e5725b71b861a690b1f4024cafe8727
Edala, Mohan <mohan.edala@anthem.com>
2019-01-20 18:14:32 -0800

    completed assignment as per requirement

---
+++ part-1-task-1.py
@@ -0,0 +1,21 @@
+int1 = 5
+int2 = 10
+int3 = 15
+int4 = 20
+int5 = 25
+int6 = 30
+int7 = 35
+int8 = 40
+int9 = 45
+int10 = 50
+
+print("int1 = ", int1);
+print("int2 = ", int2);
+print("int3 = ", int3);
+print("int4 = ", int4);
+print("int5 = ", int5);
+print("int6 = ", int6);
+print("int7 = ", int7);
+print("int8 = ", int8);
+print("int9 = ", int9);
+print("int10 = ", int10);
